90279065493277d09fe6874f6600dc9ed31cc1fc
nerdalytics <97166791+nerdalytics@users.noreply.github.com>
2026-03-15 00:00:28 +0100

    style(core): fix biome formatting in versions config

--- handbook/src/lib/versions.ts
+++ handbook/src/lib/versions.ts
@@ -1,11 +1,16 @@
 export interface Version {
+	current: boolean
 	label: string
 	prefix: string
-	current: boolean
 }
 
 export const versions: Version[] = [
-	{ current: true, label: 'v2000', prefix: '/v2000' },
+	{
+		current: true,
+		label: 'v2000',
+		prefix: '/v2000',
+	},
 ]
 
+// biome-ignore lint/style/noNonNullAssertion: versions array is static and always contains a current version
 export const currentVersion: Version = versions.find((v) => v.current)!
