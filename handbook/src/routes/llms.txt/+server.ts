feed1c06d3b94aa3616997299b0be2d2cc7e9703
nerdalytics <97166791+nerdalytics@users.noreply.github.com>
2026-04-04 10:45:44 +0200

    feat(handbook): add root llms.txt version index

---
+++ handbook/src/routes/llms.txt/+server.ts
@@ -0,0 +1,24 @@
+import type { RequestHandler } from './$types'
+import { base } from '$app/paths'
+import { versions } from '$lib/versions'
+
+export const prerender = true
+
+export const GET: RequestHandler = () => {
+	const versionLines = versions
+		.map((v) => `- [${v.label}](${base}/${v.prefix.slice(1)}/llms.txt)`)
+		.join('\n')
+
+	const body = `# Beacon
+
+> Reactive state management for JavaScript
+
+## Versions
+
+${versionLines}
+`
+
+	return new Response(body, {
+		headers: { 'Content-Type': 'text/plain; charset=utf-8' },
+	})
+}
