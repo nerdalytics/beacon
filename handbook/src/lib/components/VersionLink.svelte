674d5cfbc54b45d47c0f1478f347d0508f98b7ea
nerdalytics <97166791+nerdalytics@users.noreply.github.com>
2026-04-06 09:49:39 +0200

    feat(handbook): add VersionLink component for dynamic versioned hrefs

---
+++ handbook/src/lib/components/VersionLink.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { getContext } from 'svelte'
+	import { resolveHref } from '$lib/resolve-href'
+
+	const { prefix } = getContext('version')
+
+	export let path
+</script><a href={resolveHref(`${prefix}${path}`)}><slot /></a>
