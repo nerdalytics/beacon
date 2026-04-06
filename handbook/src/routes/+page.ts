97ca71fc037d33aeff544acb3b43d28234716224
nerdalytics <97166791+nerdalytics@users.noreply.github.com>
2026-04-06 21:13:56 +0200

    fix(handbook): redirect root to /latest instead of current version (#112)
    The root page was redirecting to /v1000.3.2 (the version marked
    current) instead of /latest. Visitors to the handbook root should
    land on /latest.

--- handbook/src/routes/+page.ts
+++ handbook/src/routes/+page.ts
@@ -1,7 +1,6 @@
 import { redirect } from '@sveltejs/kit'
 import { resolveHref } from '$lib/resolve-href'
-import { currentVersion } from '$lib/versions'
 
 export function load(): never {
-	redirect(307, resolveHref(currentVersion.prefix))
+	redirect(307, resolveHref('/latest'))
 }
